a475b1e1065a91f70e4d740df7eb9373e9a6f104
Monica Kochofar <monica@Monicas-MacBook-Air.local>
2023-03-26 16:57:51 -0400

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,7 @@ import { RouterLink, RouterView } from 'vue-router';
     </div>
   </header>
 
+  <!-- Router output -->
   <RouterView />
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
-import './assets/stylesheets/base.scss'
+import './assets/stylesheets/base.scss';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
+app.use(router);
 
-app.mount('#app')
+app.mount('#app');
