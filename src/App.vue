d2548d54033b168bda5c7448e4bc40fdbd7ba960
monicakochofar <monicakochofar@gmail.com>
2023-06-21 17:42:13 -0400

    removing external inline svg library

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router';
 import { onBeforeMount } from 'vue';
-import 'inline-svg';
 
 onBeforeMount(() => {
 
-  inlineSVG.init({
-    svgSelector: 'img.svg', // the class attached to all images that should be inlined
-    initClass: 'js-inlinesvg', // class added to <html>
-  }, function () {
-    console.log('All SVGs inlined');
-  });
+  // inlineSVG.init({
+  //   svgSelector: 'img.svg', // the class attached to all images that should be inlined
+  //   initClass: 'js-inlinesvg', // class added to <html>
+  // }, function () {
+  //   console.log('All SVGs inlined');
+  // });
 });
 
 </script>
---
+++ src/components/shared/icon.vue
@@ -0,0 +1,22 @@
+<script setup>
+  import { defineAsyncComponent } from 'vue';
+
+  const props = defineProps({
+    name: {
+      type: String,
+      required: true,
+    },
+  });
+
+  const icon = defineAsyncComponent(() =>
+    import(`../assets/svg/${props.name}.svg`)
+  );
+
+  export default {
+    icon,
+  }
+</script>
+
+<template>
+  <component :is="icon" class="fill-current" />
+</template>
