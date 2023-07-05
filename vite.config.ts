4f2e4f3d7745afb1938ef72515773832e9243f04
monicakochofar <monicakochofar@gmail.com>
2023-07-05 18:37:11 -0400

    adding public path

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,8 @@ export default defineConfig({
   plugins: [vue(), svgLoader()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     }
-  }
+  },
+  base: process.env.NODE_ENV === "production" ? "/portfolio_monica/" : "/",
 })
