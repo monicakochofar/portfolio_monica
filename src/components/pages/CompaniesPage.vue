364b7a8264a08f3925b62db8df5f4630b6917e61
monicakochofar <monicakochofar@gmail.com>
2023-07-30 02:38:25 -0400

    adding comment

--- src/components/pages/CompaniesPage.vue
+++ src/components/pages/CompaniesPage.vue
@@ -37,7 +37,7 @@ function getMonthsPerSkill(companyList) {
     });
   });
 
-  map['Java'] = map['Java'] - 2;
+  map['Java'] = map['Java'] - 2; // TODO: if you're not monica, remove this tweak
 
   return map;
 }
